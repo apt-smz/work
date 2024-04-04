58bdf45e15230ef95151efe6f1a26c6f56cf06a2
apt-smz <121246677+apt-smz@users.noreply.github.com>
2024-04-04 11:19:26 -0400

    Update kismet_mac.sh

--- kismet_mac.sh
+++ kismet_mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#this is a one shot at boot to make sure kismet starts bookworm is being funky with mediatek
 
 # Check if there are any interfaces with 'kismon' in their name
 if ! /sbin/ip link show | grep -q 'kismon'; then
