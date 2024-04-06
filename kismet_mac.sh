63abf42158de8134a20b86f37b9fa087456463ec
apt-smz <121246677+apt-smz@users.noreply.github.com>
2024-04-06 07:42:34 -0400

    Update kismet_mac.sh
    added a line for logging to show if this saved us

--- kismet_mac.sh
+++ kismet_mac.sh
@@ -4,6 +4,7 @@
 check_kismon() {
     if ! /sbin/ip link show kismon &> /dev/null; then
         systemctl restart kismet.service
+        logger "kismet_mac.sh: Restarted Kismet service because kismon interface was not found."
     fi
 }
 
